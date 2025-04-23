3b3d2812dbf8384b8b8dcf4d96aeba9f8de01557
KiefBC <kiefhay642@outlook.com>
2025-04-23 15:03:17 -0700

    fixed issue with healing items being registered as weapons and unequipping the player. added more extra areas.

--- Assets/Scripts/Combat/WeaponPickup.cs
+++ Assets/Scripts/Combat/WeaponPickup.cs
@@ -8,7 +8,7 @@ namespace Combat
 {
     public class WeaponPickup : MonoBehaviour, IRayCastable
     {
-        [FormerlySerializedAs("weapon")] [SerializeField] private WeaponConfig weaponConfig = null;
+        [SerializeField] private WeaponConfig weaponConfig = null;
         [SerializeField] private float hideTime = 5f;
         [SerializeField] private float pickupRange = 2f;
         [SerializeField] private float healthRestore = 0;
@@ -17,13 +17,23 @@ namespace Combat
         {
             if (!other.CompareTag("Player")) return;
             
-            if (other.GetComponent<Fighter>().GetWeapon() != weaponConfig)
+            // Only check weapon equality if this pickup has a weapon configuration
+            if (weaponConfig != null)
             {
-                Pickup(other.gameObject);
+                if (other.GetComponent<Fighter>().GetWeapon() != weaponConfig)
+                {
+                    Pickup(other.gameObject);
+                }
+                else
+                {
+                    Debug.Log("DEBUG :: Already have this weapon");
+                }
             }
-            else
+            else if (healthRestore > 0)
             {
-                Debug.Log("DEBUG :: Already have this weapon");
+                // If it's just a healing pickup, directly heal without weapon logic
+                other.GetComponent<Health>().Heal(healthRestore);
+                StartCoroutine(HideForSeconds(hideTime));
             }
         }
 
